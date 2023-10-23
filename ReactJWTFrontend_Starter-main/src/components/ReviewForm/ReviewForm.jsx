0ba2c3499e933e363820cb7e504b9a43dfa027b1
Colten Bowdoin <coltenbowdoin@gmail.com>
2023-10-23 17:59:45 -0500

    Worked on ReviewForm Component. Not sure how to get the correct syntax for the user story.

--- ReactJWTFrontend_Starter-main/src/components/ReviewForm/ReviewForm.jsx
+++ ReactJWTFrontend_Starter-main/src/components/ReviewForm/ReviewForm.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import React, { useState } from 'react';
+
 
 const ReviewForm = ({newReview}) => {
+  const [text, setText] = useState("");
+  const [rating, setRating] = useState();
+  const [user, setUser] = useState("");
+
+
   const handleSubmit = (event) => {
     e.preventDefault(event);
     const addReview = async () => {
@@ -8,6 +15,11 @@ const ReviewForm = ({newReview}) => {
         let response = await axios.post(
           `https://localhost:5001/api/Reveiws/${bookId}`
         );
+        let newReview = {
+          text: text,
+          rating: rating,
+          user: user
+        };
       } catch (error) {
         console.log(error);
       }
@@ -17,7 +29,9 @@ const ReviewForm = ({newReview}) => {
   return (
     <div>
       <label>Leave a Review</label>
-      <textarea value='newReview' name="textValue" onChange={handleSubmit} />
+      <textarea value={text} name="textValue" onChange={(event)=>setText(event.target.value)} />
+      <label>Rate the Book (1-5)</label>
+      <input type="number" step="any" min="0" max="5"/>
     </div>
   );
 };
